d5da8d7d737896b0568167712098bfb5dba29f28
brandami <brandami@10.0.2.15>
2017-02-01 17:48:45 +0100

    added chrome testcase and chromedriver

---
+++ myFirstSeleniumProject/src/test/java/de/selenium/firstSteps/tests/StartChromeTest.java
@@ -0,0 +1,19 @@
+package test.java.de.selenium.firstSteps.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class StartChromeTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
