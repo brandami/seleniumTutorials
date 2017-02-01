05a415ac8f0e84cd9401b228f0bf7ff74f69119e
brandami <brandami@10.0.2.15>
2017-02-01 17:34:58 +0100

    added junit testcase for starting  the chrome browser

--- myFirstSeleniumProject/src/test/java/de/selenium/firstSteps/tests/StartFirefoxTest.java
+++ myFirstSeleniumProject/src/test/java/de/selenium/firstSteps/tests/StartFirefoxTest.java
@@ -2,7 +2,7 @@ package test.java.de.selenium.firstSteps.tests;
 
 import static org.junit.Assert.fail;
 
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -12,7 +12,12 @@ public class StartFirefoxTest {
 	static String driverPath = "./ressources/geckodriver.exe";
 	public WebDriver driver;
 	
-	@BeforeClass
+	/**
+	 * @Before bedeutet, dass diese Methode vor jedem Test innerhalb dieser Klasse ausgeführt wird.
+	 * Dies wird bspw. genutzt, um notwendige Vorbedingungen für den entsprechenden Test auszuführen
+	 * @throws Exception
+	 */
+	@Before
 	public void setUp() {
 		System.out.println("*******************");
 		System.out.println("launching firefox browser");
@@ -24,8 +29,7 @@ public class StartFirefoxTest {
 
 	@Test
 	public void test() {
-		
-		
+		fail("Not yet implemented");
 	}
 
 }
