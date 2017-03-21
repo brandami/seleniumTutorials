bddfca920a621009d6f23437afd03463a583a386
brandami <michael.brandau@spirit-onside.com>
2017-03-21 17:59:59 +0100

    bugfixing in browser-project, added new ui-mapping project outsourcing

---
+++ browser/src/test/java/de/selenium/tutorial/browser/testcase/Testcase.java
@@ -0,0 +1,46 @@
+package de.selenium.tutorial.browser.testcase;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+import de.selenium.tutorial.browser.base.SeleniumBaseSpec;
+
+public class Testcase extends SeleniumBaseSpec {
+
+	public WebDriver driver;
+
+	@Test
+	public void testFirefox() {
+		driver = createDriver("Firefox");
+		driver.manage().window().maximize();
+		driver.get("https://www.google.de");
+		Assert.assertEquals("Google", driver.getTitle());
+		driver.close();
+	}
+
+	@Test
+	public void testChrome() {
+		driver = createDriver("Chrome");
+		driver.navigate().to("https://www.google.de");
+		Assert.assertEquals("Google", driver.getTitle());
+		driver.close();
+	}
+
+	@Test
+	public void testInternetExplorer() {
+		driver = createDriver("Internet Explorer");
+		driver.manage().window().maximize();
+		driver.get("https://www.google.de");
+		Assert.assertEquals("Google", driver.getTitle());
+		driver.close();
+	}
+	
+	@Test
+	public void testEdge() {
+		driver = createDriver("Edge");
+		driver.get("https://www.google.de");
+		Assert.assertEquals("Google", driver.getTitle());
+		driver.close();
+	}
+}
