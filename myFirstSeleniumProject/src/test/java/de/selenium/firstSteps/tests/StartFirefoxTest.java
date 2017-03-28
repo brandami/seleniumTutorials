ae471d7ed74ca8c2cf88c953e1a5d132e4d750c7
brandami <michael.brandau@spirit-onside.com>
2017-03-28 18:25:26 +0200

    test

--- myFirstSeleniumProject/src/test/java/de/selenium/firstSteps/tests/StartFirefoxTest.java
+++ myFirstSeleniumProject/src/test/java/de/selenium/firstSteps/tests/StartFirefoxTest.java
@@ -1,32 +1,45 @@
 package test.java.de.selenium.firstSteps.tests;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
-import org.junit.Before;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class StartFirefoxTest {
 	
-	static String driverPath = "./ressources/geckodriver.exe";
-	public WebDriver driver;
+	private static String driverPath = "./ressources/geckodriver.exe";
+	public static WebDriver driver;
 	
 	/**
 	 * @Before bedeutet, dass diese Methode vor jedem Test innerhalb dieser Klasse ausgeführt wird.
 	 * Dies wird bspw. genutzt, um notwendige Vorbedingungen für den entsprechenden Test auszuführen
 	 * @throws Exception
 	 */
-	@Before
-	public void setUp() {
+	@BeforeClass
+	public static void setUp() {
 		System.out.println("*******************");
 		System.out.println("launching firefox browser");
 		// System properties für Firefox
 		System.setProperty("webdriver.gecko.driver", driverPath);
 		driver = new FirefoxDriver();
+		//Browser maximieren
 		driver.manage().window().maximize();
+		//Sync-Timeout setzen
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 	}
 
+	@AfterClass
+	public static void tearDown() {
+		//Browser schließen
+		driver.quit();
+	}
+
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
